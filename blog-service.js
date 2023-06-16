9874c33e09c03da6d7fcad9b38e6fec53dc54f1d
ChrisZiyu <103700155+ChrisZiyu@users.noreply.github.com>
2023-06-16 17:20:22 -0400

    Assignment-3

--- blog-service.js
+++ blog-service.js
@@ -56,55 +56,109 @@ function initialize() {
       });
     });
 }
-
-
 //get all posts + error handling
-function getAllPosts(){
-    return new Promise((resolve,reject)=>{
-        if(postsData.length>0){
-            resolve(postsData);
-        } else {
-                reject("No results returned")
-            }
-        });
-    }
-    //get all published posts + error handling
-    function getPublishedPosts() {
-        return new Promise((resolve, reject) => {
-            const publishedPosts = postsData.filter(post => post.published);
-            if (publishedPosts.length > 0) {
-                resolve(publishedPosts);
-            } else {
-                reject("No results returned");
-            }
-        });
-    }
-    //get categories + error handling
-    function getCategories(){
-        return new Promise((resolve,reject)=> {
-            if(categoriesData.length>0){
+function getAllPosts() {
+    return new Promise((resolve, reject) => {
+      if (postsData.length > 0) {
+        resolve(postsData);
+      } else {
+        reject("No all results returned");
+      }
+    });
+}
+function getPublishedPosts() {
+    return new Promise((resolve, reject) => {
+      const publishedPosts = postsData.filter(post => post.published);
+      if (publishedPosts.length > 0) {
+        resolve(publishedPosts);
+      } else {
+        reject("No published return");
+      }
+    });
+}
+function getPostsByCategory(category) {
+    return new Promise((resolve, reject) => {
+      const postsWithCategory = postsData.filter(post => post.category === parseInt(category));
+      const postsContent = postsData.filter(post => post.category === category);
+      if (postsContent || postsWithCategory.length > 0) {
+        const mergedPosts=[...postsContent, ...postsWithCategory]
+        resolve(mergedPosts);
+      } else {
+        reject("No posts found with the specified category");
+      }
+    });
+  }
+  
+  function getPostsByMinDate(minDateStr) {
+    return new Promise((resolve, reject) => {
+      const minDate = new Date(minDateStr);
+      const postsByMinDate = postsData.filter(post => new Date(post.postDate) >= minDate);
+      if (postsByMinDate.length > 0) {
+        resolve(postsByMinDate);
+      } else {
+        reject("No results returned");
+      }
+    });
+  }
+  function getPostById(id) {
+    return new Promise((resolve, reject) => {
+      const post = postsData.find(post => post.id === id);
+      if (post) {
+        resolve(post);
+      } else {
+        reject("No result returned");
+      }
+    });
+  }
+//get categories + error handling
+function getCategories(){
+    return new Promise((resolve,reject)=> {
+        if(categoriesData.length>0){
                 resolve(categoriesData);
-            }else{
-                reject("No results returned")
-            }
+        }else{
+            reject("aaa")
+        }
+    });
+}
+
+    function addPost(postData) {
+        return new Promise((resolve, reject) => {
+          // If postData.published is undefined, set it to false; otherwise, set it to true
+          postData.published = postData.published === undefined ? false : true;
+      
+          // Set the id property of postData to be the length of the "postsData" array plus one
+          postData.id = postsData.length + 1;
+      
+          // Push the updated postData to the "postsData" array
+          postsData.push(postData);
+      
+          // Resolve the promise with the updated value
+          resolve(postData);
         });
     }
+
     
     // Route functions
-        function allPosts() {
-            return JSON.stringify(postsData);
-        }
+     function allPosts() {
+       return JSON.stringify(postsData);
+    }
     
-        function allCategories() {
+    function allCategories() {
         return JSON.stringify(categoriesData);
-        }
+    }
+
+    
     
 module.exports = {
     initialize,
     getAllPosts,
     getPublishedPosts,
+    getPostsByCategory,
+    getPostsByMinDate,
+    getPostById,
     getCategories,
     allPosts,
-    allCategories
+    allCategories,
+    addPost,
     };
           
--- server.js
+++ server.js
@@ -14,7 +14,17 @@
 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
+const path = require("path");
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 var app = express();
+
+cloudinary.config({ 
+  cloud_name: 'dwup3hili', 
+  api_key: '777482753413527', 
+  api_secret: '4rrEmjOUpae1UoGa4n4Jfbw6y5o' 
+});
 
 const blogService = require("./blog-service");
 
@@ -72,6 +82,80 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/views/about.html");
 });
+// Route for returning the addPost.html file
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/addPost.html"));
+});
+
+//multer for posts add only
+const upload = multer(); //remember to move if more posts coming for next assignments
+
+app.post('/posts/add', upload.single('featureImage'), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function uploadImage(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    uploadImage(req)
+      .then((uploaded) => {
+        processPost(uploaded.url);
+      })
+      .catch((error) => {
+        console.error('Error uploading image:', error);
+        // Handle the error and redirect to an error page or display an error message
+        res.redirect('/error');
+      });
+  } else {
+    processPost('');
+  }
+
+  function processPost(imageUrl) {
+    var category = req.body.category;
+    req.body.featureImage = imageUrl;
+    // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+    var published = req.body.published === undefined ? false : true;
+    // process the req.body and add it as a new Blog Post
+    const newBlogPost={
+      title: req.body.title,
+      body:req.body.content,
+      postDate: new Date().toISOString(),
+      category:category,
+      featureImage: imageUrl,
+      published: published,
+      
+    };
+    blogService
+      .addPost(newBlogPost)
+      .then((addedPost) => {
+        console.log('New blog post added:', addedPost);
+        res.redirect('/posts');
+      })
+      .catch((error) => {
+        console.log('Error adding blog post:', error);
+        res.redirect('/posts');
+      });
+
+    // // Redirect to /posts or wherever you want to redirect after adding the post
+    // res.redirect('/posts');
+  }
+});
+
+
 
 
 // Route for returning published posts
@@ -85,16 +169,56 @@ app.get("/blog", (req, res) => {
     });
 });
   
+app.get("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+
+  blogService
+    .getPostById(postId)
+    .then(post => {
+      res.send(JSON.stringify(post));
+    })
+    .catch(error => {
+      res.status(500).send("Error: " + error);
+    });
+});
 // Route for returning all posts
+// Updated /posts route
 app.get("/posts", (req, res) => {
-    blogService.getAllPosts()
+  const category = req.query.category;
+  const minDate = req.query.minDate;
+
+  if (category) {
+    blogService
+      .getPostsByCategory(category)
+      .then(filteredPosts => {
+        res.send(JSON.stringify(filteredPosts));
+      })
+      .catch(error => {
+        res.status(500).send("Error: " + error);
+      });
+  } else if (minDate) {
+    blogService
+      .getPostsByMinDate(minDate)
+      .then(filteredPosts => {
+        res.send(JSON.stringify(filteredPosts));
+      })
+      .catch(error => {
+        res.status(500).send("Error: " + error);
+      });
+  } else {
+    blogService
+      .getAllPosts()
       .then(posts => {
         res.send(JSON.stringify(posts));
       })
       .catch(error => {
         res.status(500).send("Error: " + error);
       });
-  });
+  }
+});
+
+
+
 
 // Route for returning all categories
 app.get("/categories", (req, res) => {
@@ -103,7 +227,7 @@ app.get("/categories", (req, res) => {
       res.send(JSON.stringify(Categories));
     })
     .catch(error => {
-      res.status(500).send("Error: " + error);
+      res.status(404).send("Error: " + error);
     });
 });
   
